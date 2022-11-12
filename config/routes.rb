d5a97497803946a9b54d264ee42d79665610f4b0
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2022-11-12 20:59:30 +0000

    todo_items controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :todo_lists
+  resources :todo_lists do 
+    resources :todo_items
+  end
+
   get 'about', to:'home#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
