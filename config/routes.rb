624136ed26b628e89dd16cb49a62e16482b06422
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2022-11-12 22:00:21 +0000

    revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :todo_lists do 
     resources :todo_items
   end
 
+=======
+  resources :tasks
+>>>>>>> parent of f333e4c (deleted scaffold TASK)
   get 'about', to:'home#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
